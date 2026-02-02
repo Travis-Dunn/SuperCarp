77b63fbfcfe55f858695a3bc965eec4108997af9
Travis-Dunn <t_a_dunn@proton.me>
2026-02-01 22:29:36 -0500

    DisplayConfig wip

--- src/main/java/production/DisplayConfig.java
+++ src/main/java/production/DisplayConfig.java
@@ -12,49 +12,115 @@ public final class DisplayConfig {
 
     /* 'display' Refers to the display resolution that the OS sees */
     private static int displayW, displayH;
-    /* 'framebuffer' Refers to the software emulated sub-scale framebuffer */
-    private static int framebufferW, framebufferH;
-    /* 'viewport' Refers to the "game window", I.E., not the space in the window
-        dedicated to the UI */
+    /* 'window' Refers to the window resolution that the OS sees */
+    private static int windowW, windowH;
+    /* 'emulated' Refers to the software rendered framebuffer */
+    private static int emulatedW, emulatedH;
+    /* 'viewport' Refers to the "game window", I.E., not dedicated UI space. */
     private static int viewportW, viewportH;
-    /* Ratio of display pixels to framebuffer pixels */
+    /* Ratio of display pixels to emulated pixels */
     private static int iPixelScale;
     private static boolean bFullscreen;
 
     /**
-     * Ingests requested display properties (Probably from config file), queries
-     * physical display information from OS, and uses them to determine the
-     * actual display properties.
+     * Ingests requested display properties, and queries physical display
+     * information from OS, and uses them to determine the actual display
+     * properties.
+     *
+     * Pass only:
+     * One of [fullscreen, window resolution]
+     * And
+     * One of [pixel scale, emulated resolution]
+     * The rest must be 0 or false.
      *
      * @param bFullscreen true if fullscreen is desired
      * @param iPixelScale ratio of screen pixels to framebuffer pixels
+     * @param emulatedW width of the emulated display
+     * @param emulatedH height of the emulated display
+     * @param windowW width of the application window
+     * @param windowH height of the application window.
      * @return true is success
      */
-    public static boolean Init(boolean bFullscreen, int iPixelScale) {
-        assert(!init);
+    public static boolean Init(
+            boolean bFullscreen,
+            int iPixelScale,
+            int emulatedW,
+            int emulatedH,
+            int windowW,
+            int windowH) {
+        assert (!init);
+
+        LogSession(LogLevel.DEBUG, CLASS + " initializing...\n");
 
         DisplayMode desktop = Display.getDesktopDisplayMode();
         displayW = desktop.getWidth();
         displayH = desktop.getHeight();
 
-        if (!(iPixelScale >= 0)) {
+        if (iPixelScale < 0) {
             LogFatalAndExit(ERR_STR_INIT_FAILED_PIXEL_SCALE_NEG);
-            return false;
-        } else if (iPixelScale == 0) {
-            DisplayConfig.iPixelScale = AutoCalcPixelScale();
-        } else if (displayW % iPixelScale != 0 || displayH % iPixelScale != 0) {
+            return init = false;
+        }
+
+        DisplayConfig.bFullscreen = bFullscreen;
+
+        if (bFullscreen && windowW == 0 && windowH == 0) {
+            if (emulatedW == 0 && emulatedH == 0 && iPixelScale != 0) {
+                init = InitMode0(iPixelScale);
+            } else if (emulatedW != 0 && emulatedH != 0 && iPixelScale == 0) {
+                init = InitMode1();
+            }
+        } else if (!bFullscreen && windowW != 0 && windowH != 0) {
+            if (emulatedW == 0 && emulatedH == 0 && iPixelScale != 0) {
+                init = InitMode2();
+            } else if (emulatedW != 0 && emulatedH != 0 && iPixelScale == 0) {
+                init = InitMode3();
+            }
+        } else {
+            LogFatalAndExit(ErrStrFailedInitIncompatibleParams());
+            return init = false;
+        }
+
+        SetViewport();
+
+        if (init) LogSession(LogLevel.DEBUG, ERR_STR_INIT);
+
+        return init;
+    }
+
+    /***
+     * Create fullscreen config given pixel scale.
+     *
+     * Display resolution must be a multiple of pixel scale.
+     */
+    private static boolean InitMode0(int iPixelScale) {
+        if (!(displayW % iPixelScale == 0 && displayH % iPixelScale == 0)) {
             LogFatalAndExit(ErrStrInitFailedPixelScale());
             return false;
         } else {
             DisplayConfig.iPixelScale = iPixelScale;
+            windowW = displayW;
+            windowH = displayH;
+            emulatedW = displayW / DisplayConfig.iPixelScale;
+            emulatedH = displayH / DisplayConfig.iPixelScale;
         }
 
-        LogSession(LogLevel.DEBUG, LogStrDetectedMonitor());
+        return true;
+    }
 
-        /* TODO: add "starting..." mesgs and calc the other vals */
+    /***
+     * Create fullscreen config given emulated resolution.
+     *
+     * Display resolution must be a multiple of emulated resolution.
+     */
+    private static boolean InitMode1() {
+        return true;
+    }
 
-        DisplayConfig.bFullscreen = bFullscreen;
+    private static boolean InitMode2() {
+        return true;
+    }
 
+    private static boolean InitMode3() {
         return true;
     }
 
@@ -67,11 +133,28 @@ public final class DisplayConfig {
         }
     }
 
-    private static final String CLASS= DisplayConfig.class.getSimpleName();
-    private static String LogStrDetectedMonitor() {
-        return String.format("%s detected a display with width [%d] and " +
-                "height [%d].\n", CLASS, displayW, displayH);
+    /***
+     * Explicitly handles known combinations, otherwise sets viewport to two
+     * thirds of framebuffer.
+     */
+    private static void SetViewport() {
+        if (emulatedW == 480 && displayH == 270) {
+            viewportW = 320;
+            viewportH = 180;
+        } else {
+            viewportW = (emulatedW / 3) * 2;
+            viewportH = (emulatedH / 3) * 2;
+        }
     }
+
+    private static final String CLASS= DisplayConfig.class.getSimpleName();
+    private static final String ERR_STR_INIT = CLASS + " initialized with " +
+            "display width [" + displayW + "], and display height [" +
+            displayH + "]\n frame buffer width [" + emulatedW + "], and " +
+            "framebuffer height [" + emulatedH + "]\n viewport port width " +
+            "[" + viewportW + "], and viewport height [" + viewportH + "]\n" +
+            "pixel scale [" + iPixelScale + "], and " +
+            (bFullscreen ? "full screen" : "windowed") + " mode.\n";
     private static final String ERR_STR_INIT_FAILED_PIXEL_SCALE_NEG = CLASS +
             " failed to initialize because iPixelScale was negative.\n";
     private static String ErrStrInitFailedPixelScale() {
@@ -84,5 +167,8 @@ public final class DisplayConfig {
                 " automatically calculate iPixelScale for a display with " +
                 "width [%d] and height [%d], but could not find a suitable " +
                 "preset.\n", CLASS, displayW, displayH);
+    }
+    private static String ErrStrFailedInitIncompatibleParams() {
+        return String.format("");
     }
 }
